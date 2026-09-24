7d235089e7af8b71d93baefaf217d30f84a0111c
aodhanwynndale <aodhanwynndale@gmail.com>
2026-09-24 22:06:29 +0700

    Update july.js

--- data/cycle-1/july.js
+++ data/cycle-1/july.js
@@ -1,1 +1,319 @@
-// July — Cycle I
+const julyStudents = [
+  {
+        name:
+            "Ryusarion Sinclair",
+
+        month:
+            "July",
+
+        rank:
+            1,
+
+        type:
+            "BLUE",
+
+        identity:
+            "2022 · @arcxaeden_",
+
+        photo:
+            "images/ryusarion.jpg",
+
+        quote:
+            "",
+
+        x:
+            789,
+
+        y:
+            340
+    },
+
+
+    {
+        name:
+            "Mireilisse de Castellane",
+
+        month:
+            "July",
+
+        rank:
+            2,
+
+        type:
+            "ORANGE",
+
+        identity:
+            "5024 · @lunefleuir",
+
+        photo:
+            "images/mireilisse.jpg",
+
+        quote:
+            "",
+
+        x:
+            302,
+
+        y:
+            296
+    },
+
+
+    {
+        name:
+            "Naja Kusunoki",
+
+        month:
+            "July",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4026 · @crackstne",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            382,
+
+        y:
+            261
+    },
+
+
+    {
+        name:
+            "Seraphina Marsh",
+
+        month:
+            "July",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4029 · @etreveire",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            222,
+
+        y:
+            173
+    },
+
+
+    {
+        name:
+            "Soleil Nocturne",
+
+        month:
+            "July",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2002 · @slIaughter",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            367,
+
+        y:
+            96
+    },
+
+
+    {
+        name:
+            "Vangelyn Bloom",
+
+        month:
+            "July",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2008 · @obscendio",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            541,
+
+        y:
+            403
+    },
+
+
+    {
+        name:
+            "Zachary Goth",
+
+        month:
+            "July",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2023 · @adzachtive",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            533,
+
+        y:
+            186
+    },
+
+
+    {
+        name:
+            "Catriona Morvane",
+
+        month:
+            "July",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6006 · @lilrevernel",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            336,
+
+        y:
+            405
+    },
+
+
+    {
+        name:
+            "Lichelyn Mielle",
+
+        month:
+            "July",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6029 · @itsxmiel",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            697,
+
+        y:
+            450
+    },
+
+
+    {
+        name:
+            "Desirée Hope",
+
+        month:
+            "July",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2003 · @dolceir",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            742,
+
+        y:
+            229
+    },
+];
+
+const julyLines = [
+  [
+    "Mireilisse de Castellane",
+    "Seraphina Marsh"
+],
+
+[
+    "Seraphina Marsh",
+    "Naja Kusunoki"
+],
+
+[
+    "Naja Kusunoki",
+    "Ryusarion Sinclair"
+],
+
+    ],
+];
