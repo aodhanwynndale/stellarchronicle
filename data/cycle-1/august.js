d2f791f4f52373744ff4876a0cbc504dc51d16d3
aodhanwynndale <aodhanwynndale@gmail.com>
2026-09-24 22:09:10 +0700

    Update august.js

--- data/cycle-1/august.js
+++ data/cycle-1/august.js
@@ -1,1 +1,484 @@
-// August — Cycle I
+const augustStudents = [
+  {
+        name:
+            "Richard Redmayne",
+
+        month:
+            "August",
+
+        rank:
+            1,
+
+        type:
+            "BLUE",
+
+        identity:
+            "5025 · @spikemoth",
+
+        photo:
+            "images/richard.jpg",
+
+        quote:
+            "",
+
+        x:
+            427,
+
+        y:
+            171
+    },
+
+
+    {
+        name:
+            "Vangelyn Bloom",
+
+        month:
+            "August",
+
+        rank:
+            2,
+
+        type:
+            "ORANGE",
+
+        identity:
+            "2008 · @obscendio",
+
+        photo:
+            "images/vangelyn.jpg",
+
+        quote:
+            "",
+
+        x:
+            454,
+
+        y:
+            130
+    },
+
+
+    {
+        name:
+            "Mireilisse de Castellane",
+
+        month:
+            "August",
+
+        rank:
+            3,
+
+        type:
+            "RED",
+
+        identity:
+            "5024 · @lunefleuir",
+
+        photo:
+            "images/mireilisse.jpg",
+
+        quote:
+            "",
+
+        x:
+            307,
+
+        y:
+            396
+    },
+
+
+    {
+        name:
+            "Vaelric Night",
+
+        month:
+            "August",
+
+        rank:
+            3,
+
+        type:
+            "RED",
+
+        identity:
+            "5015 · @lanternsdim",
+
+        photo:
+            "images/vaelric.jpg",
+
+        quote:
+            "",
+
+        x:
+            686,
+
+        y:
+            57
+    },
+
+
+    {
+        name:
+            "Maybeth Prince",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2019 · @e3vrne",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            572,
+
+        y:
+            302
+    },
+
+
+    {
+        name:
+            "Celestine Zefraind",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4013 · @DEATHLYSlNNER",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            687,
+
+        y:
+            496
+    },
+
+
+    {
+        name:
+            "Naja Kusunoki",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4026 · @crackstne",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            401,
+
+        y:
+            227
+    },
+
+
+    {
+        name:
+            "Vivienne-Aurélie Vestergaard",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "5041 · @Mirthinlace",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            421,
+
+        y:
+            351
+    },
+
+
+    {
+        name:
+            "Lichelyn Mielle",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6029 · @talesoflele",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            724,
+
+        y:
+            23
+    },
+
+
+    {
+        name:
+            "Claude Morvane",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2012 · @SonnetOfDusk",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            510,
+
+        y:
+            408
+    },
+
+
+    {
+        name:
+            "Clèa Reese",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6007 · @ephxmcre",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            719,
+
+        y:
+            510
+    },
+
+
+    {
+        name:
+            "Evanthe Yūtenji",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6034 · @malsondiev",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            528,
+
+        y:
+            204
+    },
+
+
+    {
+        name:
+            "Ryusarion Sinclair",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "5022 · @arcxaeden_",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            759,
+
+        y:
+            335
+    },
+
+
+    {
+        name:
+            "Kawakubo Shinji",
+
+        month:
+            "August",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6015 · @cinetsugi",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            659,
+
+        y:
+            270
+    },
+  ];
+const augustLines = [
+   [
+    "Clèa Reese",
+    "Celestine Zefraind"
+],
+
+[
+    "Celestine Zefraind",
+    "Maybeth Prince"
+],
+
+[
+    "Celestine Zefraind",
+    "Claude Morvane"
+],
+
+[
+    "Claude Morvane",
+    "Mireilisse de Castellane"
+],
+
+[
+    "Mireilisse de Castellane",
+    "Vivienne-Aurélie Vestergaard"
+],
+
+[
+    "Vivienne-Aurélie Vestergaard",
+    "Maybeth Prince"
+],
+
+[
+    "Maybeth Prince",
+    "Vangelyn Bloom"
+],
+
+[
+    "Vangelyn Bloom",
+    "Richard Redmayne"
+],
+
+[
+    "Richard Redmayne",
+    "Naja Kusunoki"
+],
+
+[
+    "Maybeth Prince",
+    "Vaelric Night"
+],
+
+[
+    "Celestine Zefraind",
+    "Vaelric Night"
+],
+
+[
+    "Vaelric Night",
+    "Lichelyn Mielle"
+],
+
+
+    ],
+  ];
