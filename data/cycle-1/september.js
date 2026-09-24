14264fec37f1a691bda64dcb7421a184798c74dc
aodhanwynndale <aodhanwynndale@gmail.com>
2026-09-24 22:13:30 +0700

    Update september.js

--- data/cycle-1/september.js
+++ data/cycle-1/september.js
@@ -1,1 +1,546 @@
-// September — Cycle I
+const septemberStudents = [
+   {
+        name:
+            "Vaelric Night",
+
+        month:
+            "September",
+
+        rank:
+            1,
+
+        type:
+            "BLUE",
+
+        identity:
+            "5015 · @fireswither",
+
+        photo:
+            "images/vaelric.jpg",
+
+        quote:
+            "",
+
+        x:
+            451,
+
+        y:
+            193
+    },
+
+
+    {
+        name:
+            "Vivienne-Aurélie Vestergaard",
+
+        month:
+            "September",
+
+        rank:
+            2,
+
+        type:
+            "ORANGE",
+
+        identity:
+            "5041 · @Havenstoria",
+
+        photo:
+            "images/vivienne.jpg",
+
+        quote:
+            "",
+
+        x:
+            504,
+
+        y:
+            293
+    },
+
+
+    {
+        name:
+            "Serica Yūtenji",
+
+        month:
+            "September",
+
+        rank:
+            3,
+
+        type:
+            "RED",
+
+        identity:
+            "5025 · @spikemoth",
+
+        photo:
+            "images/serica.jpg",
+
+        quote:
+            "",
+
+        x:
+            398,
+
+        y:
+            438
+    },
+
+
+    {
+        name:
+            "Maybeth Prince",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2019 · @starlitrd",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            669,
+
+        y:
+            222
+    },
+
+
+    {
+        name:
+            "Kawakubo Shinji",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6015 · @cinetelier",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            750,
+
+        y:
+            542
+    },
+
+
+    {
+        name:
+            "Celestine Zefraind",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4013 · @puingpatah",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            269,
+
+        y:
+            483
+    },
+
+
+    {
+        name:
+            "Keiranza Birkins",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "5006 · @vourione",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            772,
+
+        y:
+            56
+    },
+
+
+    {
+        name:
+            "Rena Kisaragi",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6034 · @mahoumiru",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            549,
+
+        y:
+            165
+    },
+
+
+    {
+        name:
+            "Claude Morvane",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2012 · @SonnetOfDusk",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            720,
+
+        y:
+            95
+    },
+
+
+    {
+        name:
+            "Mireilisse de Castellane",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "5024 · @lluminea",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            630,
+
+        y:
+            407
+    },
+
+
+    {
+        name:
+            "Naja Kusunoki",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4026 · @ethealreals",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            376,
+
+        y:
+            270
+    },
+
+
+    {
+        name:
+            "Clèa Reese",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6007 · @ephxmcre",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            828,
+
+        y:
+            283
+    },
+
+
+    {
+        name:
+            "Kennedict Morvane",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4037 · @edsanctus",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            222,
+
+        y:
+            287
+    },
+
+
+    {
+        name:
+            "Valentine Night",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4044 · @VeilOfChamber",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            556,
+
+        y:
+            493
+    },
+
+
+    {
+        name:
+            "Valtherion Night",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6028 · @ItMightEnd",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            469,
+
+        y:
+            78
+    },
+
+
+    {
+        name:
+            "Ryusarion Whitmore",
+
+        month:
+            "September",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6028 · @arcxaeden_",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            303,
+
+        y:
+            122
+    },
+
+];
+
+const septemberLines = [
+  [
+    "Naja Kusunoki",
+    "Celestine Zefraind"
+],
+
+[
+    "Celestine Zefraind",
+    "Serica Yūtenji"
+],
+
+[
+    "Serica Yūtenji",
+    "Vivienne-Aurélie Vestergaard"
+],
+
+[
+    "Naja Kusunoki",
+    "Vaelric Night"
+],
+
+[
+    "Vaelric Night",
+    "Vivienne-Aurélie Vestergaard"
+],
+
+[
+    "Vaelric Night",
+    "Rena Kisaragi"
+],
+
+[
+    "Rena Kisaragi",
+    "Claude Morvane"
+],
+
+[
+    "Maybeth Prince",
+    "Claude Morvane"
+],
+
+[
+    "Claude Morvane",
+    "Keiranza Birkins"
+],
+
+[
+    "Vivienne-Aurélie Vestergaard",
+    "Maybeth Prince"
+],
+
+[
+    "Vivienne-Aurélie Vestergaard",
+    "Mireilisse de Castellane"
+],
+
+[
+    "Mireilisse de Castellane",
+    "Kawakubo Shinji"
+],
+
+    ],
+
+];
