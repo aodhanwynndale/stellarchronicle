243e0d5b33fd800ce6ae56ef209eb4edc177290d
aodhanwynndale <aodhanwynndale@gmail.com>
2026-09-24 21:45:19 +0700

    Update june.js

--- data/cycle-1/june.js
+++ data/cycle-1/june.js
@@ -1,1 +1,339 @@
-// June — Cycle I
+const juneStudents = [
+ {
+        name:
+            "Nicholette Rhodes",
+
+        month:
+            "June",
+
+        rank:
+            1,
+
+        type:
+            "BLUE",
+
+        identity:
+            "4022 · @poetiacs",
+
+        photo:
+            "images/nicholette.jpg",
+
+        quote:
+            "A brilliant northern light.",
+
+        x:
+            685,
+
+        y:
+            163
+    },
+
+
+    {
+        name:
+            "Naja Kusunoki",
+
+        month:
+            "June",
+
+        rank:
+            2,
+
+        type:
+            "ORANGE",
+
+        identity:
+            "4026 · @crackstne",
+
+        photo:
+            "images/naja.jpg",
+
+        quote:
+            "A warm guardian of the northern bowl.",
+
+        x:
+            361,
+
+        y:
+            264
+    },
+
+
+    {
+        name:
+            "Kasper Goth",
+
+        month:
+            "June",
+
+        rank:
+            3,
+
+        type:
+            "RED",
+
+        identity:
+            "5003 · @hatchscape",
+
+        photo:
+            "images/kasper.jpg",
+
+        quote:
+            "A vivid point along the Little Dipper.",
+
+        x:
+            415,
+
+        y:
+            289
+    },
+
+
+    {
+        name:
+            "Mireilisse de Castellane",
+
+        month:
+            "June",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "5024 · @lluminea",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            597,
+
+        y:
+            169
+    },
+
+
+    {
+        name:
+            "Desirée Hope",
+
+        month:
+            "June",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2003 · @dolceir",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            504,
+
+        y:
+            201
+    },
+
+
+    {
+        name:
+            "Ryusarion Sinclair",
+
+        month:
+            "June",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "2022 · @arcxaeden_",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            312,
+
+        y:
+            378
+    },
+
+
+    {
+        name:
+            "Seraphina Marsh",
+
+        month:
+            "June",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "4029 · @eufroosine",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            388,
+
+        y:
+            398
+    },
+
+
+    {
+        name:
+            "Vivienne-Aurélie Vestergaard",
+
+        month:
+            "June",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "5041 · @mirthinlace",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            633,
+
+        y:
+            296
+    },
+
+
+    {
+        name:
+            "Clèa Reese",
+
+        month:
+            "June",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "6007 · @clealaire",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            469,
+
+        y:
+            459
+    },
+
+
+    {
+        name:
+            "Magnolia Daisy",
+
+        month:
+            "June",
+
+        rank:
+            0,
+
+        type:
+            "WHITE",
+
+        identity:
+            "5028 · @insolcent",
+
+        photo:
+            "",
+
+        quote:
+            "",
+
+        x:
+            386,
+
+        y:
+            134
+    },
+];
+
+const juneLines = [
+
+        [
+            "Nicholette Rhodes",
+            "Mireilisse de Castellane"
+        ],
+
+        [
+            "Mireilisse de Castellane",
+            "Desirée Hope"
+        ],
+
+        [
+            "Desirée Hope",
+            "Kasper Goth"
+        ],
+
+        [
+            "Kasper Goth",
+            "Naja Kusunoki"
+        ],
+
+        [
+            "Naja Kusunoki",
+            "Ryusarion Sinclair"
+        ],
+
+        [
+            "Ryusarion Sinclair",
+            "Seraphina Marsh"
+        ],
+
+        [
+          "Kasper Goth",
+          "Seraphina Marsh"
+        ]
+
+   ];
